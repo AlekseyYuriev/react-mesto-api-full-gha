83504adf8fb14867a694aba7d3505e629b1f0dad
Aleksey Yuriev <alex11-90@yandex.by>
2023-12-05 21:34:41 +0300

    hotfix: eslint config added

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,18 +1,17 @@
-
 const allowedCors = [
   'https://localhost:3000',
   'http://localhost:3000',
   'https://api.mestominsk.nomoredomainsmonster.ru',
   'http://api.mestominsk.nomoredomainsmonster.ru',
   'https://mestominsk.nomoredomainsmonster.ru',
-  'http://mestominsk.nomoredomainsmonster.ru'
+  'http://mestominsk.nomoredomainsmonster.ru',
 ];
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
 
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
